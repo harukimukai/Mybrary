5c02504d2bd2c4d19595df2cf605e1600ed4a26a
Haruki Mukai <mukaiyousei@mukaiyouseisMBP.lan>
2025-02-23 10:06:51 -0600

    Finished Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Author = require('../models/author')
+const Book = require('../models/book')
 const router = express.Router()
 
 // All Authors Route
@@ -32,12 +33,76 @@ router.post('/', async (req, res) => {
 
     try {
         const newAuthor = await author.save()
-        res.redirect(`authors`)
+        res.redirect(`authors/${newAuthor.id}`)
     } catch {
         res.render('authors/new',{
             author: author,
             errorMessage:'Error Creating Author...'
         })
+    }
+})
+
+//Show author
+router.get('/:id', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        const books = await Book.find({ author: author.id }).limit(6).exec()
+        res.render('authors/show', {
+            author: author,
+            booksByAuthor: books
+        })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
+//Edit author
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', { author: author })
+    } catch {
+        res.redirect('/authors')
+    }
+    
+})
+
+//Update author
+router.put('/:id', async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+    } catch {
+        if (author == null) {
+            res.redirect('/')
+            console.log('Cannot find the author by id')
+        } else {
+            res.render('authors/edit',{
+                author: author,
+                errorMessage:'Error updating Author...'
+            })
+        }
+    }
+})
+
+//Delete author
+router.delete('/:id', async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.deleteOne()
+        res.redirect('/authors')
+    } catch {
+        if (author == null) {
+            res.redirect('/')
+            console.log('Cannot find the author by id')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
     }
 })
 
